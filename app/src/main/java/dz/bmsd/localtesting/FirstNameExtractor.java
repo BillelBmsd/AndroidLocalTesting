766034437dc17c4ab6dfe7160de899a1437e7371
billel boumessaidia <billel.boumessaidia@cevital.com>
2019-05-29 15:08:55 +0100

    add first name unit test

---
+++ app/src/main/java/dz/bmsd/localtesting/FirstNameExtractor.java
@@ -0,0 +1,23 @@
+package dz.bmsd.localtesting;
+
+import android.util.Log;
+
+public class FirstNameExtractor {
+    private static final String TAG = "FirstNameExtractor";
+
+    public static String extractFirstName(String fullName){
+
+        if (fullName == null || fullName.isEmpty()) {
+            return "";
+        }
+
+        String[] split = fullName.split(" ");
+        for (String word : split) {
+            if (!word.isEmpty()) {
+                return word;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ app/src/test/java/dz/bmsd/localtesting/FirstNameExtractorTest.java
@@ -0,0 +1,64 @@
+package dz.bmsd.localtesting;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static dz.bmsd.localtesting.FirstNameExtractor.*;
+
+public class FirstNameExtractorTest {
+
+    private static String BMSD="Bmsd";
+
+    @Test
+    public void extractFirstName_NullInput_ReturnEmptyString() {
+        assertThat(extractFirstName(null),is(""));
+    }
+
+    @Test
+    public void extractFirstName_EmptyInput_ReturnEmptyString(){
+        assertThat(extractFirstName(""),is(""));
+    }
+
+    @Test
+    public void extractFirstName_FullName_ReturnsCorrect() {
+        assertThat(extractFirstName("Bmsd Billel"), is(BMSD));
+    }
+
+    @Test
+    public void extractFirstName_FullNameAroundWhiteSpaces_ReturnsCorrect() {
+        assertThat(extractFirstName("Bmsd Billel "), is(BMSD));
+        assertThat(extractFirstName(" Bmsd Billel"), is(BMSD));
+        assertThat(extractFirstName("Bmsd    Billel"), is(BMSD));
+        assertThat(extractFirstName("   Bmsd Billel   "), is(BMSD));
+        assertThat(extractFirstName(" Bmsd Billel  "), is(BMSD));
+    }
+
+    @Test
+    public void extractFirstName_FirstName_ReturnsCorrect() {
+        assertThat(extractFirstName("Bmsd"), is(BMSD));
+    }
+
+    @Test
+    public void extractFirstName_FirstNameAroundWhiteSpaces_ReturnsCorrect() {
+        assertThat(extractFirstName("Bmsd "), is(BMSD));
+        assertThat(extractFirstName(" Bmsd"), is(BMSD));
+        assertThat(extractFirstName(" Bmsd "), is(BMSD));
+        assertThat(extractFirstName("  Bmsd   "), is(BMSD));
+    }
+
+    @Test
+    public void extractFirstName_ThreeWords_ReturnsCorrect() {
+        assertThat(extractFirstName("Bmsd Bmsd Billel"), is(BMSD));
+    }
+
+    @Test
+    public void extractFirstName_ThreeWordsAroundWhiteSpaces_ReturnsCorrect() {
+        assertThat(extractFirstName("Bmsd Bmsd Billel "), is(BMSD));
+        assertThat(extractFirstName(" Bmsd Bmsd Billel"), is(BMSD));
+        assertThat(extractFirstName("   Bmsd Bmsd Billel   "), is(BMSD));
+        assertThat(extractFirstName("Bmsd   Bmsd   Billel"), is(BMSD));
+        assertThat(extractFirstName("  Bmsd   Bmsd   Billel"), is(BMSD));
+        assertThat(extractFirstName("  Bmsd   Bmsd   Billel   "), is(BMSD));
+    }
+}
