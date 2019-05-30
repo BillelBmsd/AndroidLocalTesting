b758057eadd9488d79cfabc64246d687980aae51
billel boumessaidia <billel.boumessaidia@cevital.com>
2019-05-30 12:12:51 +0100

    add json parser unit test

---
+++ app/src/main/java/dz/bmsd/localtesting/Location.java
@@ -0,0 +1,40 @@
+package dz.bmsd.localtesting;
+
+public class Location {
+
+    private String cityName;
+    private double latitude, langitude;
+
+    public Location() {
+    }
+
+    public Location(String cityName, double latitude, double langitude) {
+        this.cityName = cityName;
+        this.latitude = latitude;
+        this.langitude = langitude;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return langitude;
+    }
+
+    public void setLangitude(double langitude) {
+        this.langitude = langitude;
+    }
+}
